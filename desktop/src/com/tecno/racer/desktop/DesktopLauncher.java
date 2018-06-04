3f89bf6ed35a9435ad28e7f0da5dd1b6563f7e33
Joseph Vargas <joseph@cpokemon.com>
2018-06-03 18:57:48 -0600

    First commit

---
+++ desktop/src/com/tecno/racer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tecno.racer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tecno.racer.TecnoRacer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TecnoRacer(), config);
+	}
+}
